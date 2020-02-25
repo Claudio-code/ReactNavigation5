ff0795c5e38db713ef9760fb1e4353f336483930
claudio-code <suporte2424@outlook.com>
2020-02-25 19:21:11 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>dedqsw</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# TestReactNavigation5
