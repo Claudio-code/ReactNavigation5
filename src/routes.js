143779806d0d46b948d67b04f4f7781c737695e6
claudio-code <suporte2424@outlook.com>
2020-02-26 02:02:29 -0300

    configurando as rotas da  materialTopTabNavigator

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 import FacebookDashboard from './pages/FacebookDashboard';
 import YoutubeDashboard from './pages/YoutubeDashboard';
 
-import HeaderFace from './components/Headers/HeaderFace/index';
+import HeaderFace from './components/Headers/HeaderFace';
+import { color } from 'react-native-reanimated';
 
 const FaceStack = createStackNavigator();
 const YoutubeStack = createStackNavigator();
 const BottomTabsStack = createBottomTabNavigator();
-
+const TopTabsStack = createMaterialTopTabNavigator();
+const RootStack = createStackNavigator();
 
 const Face = () => {
   return (
     <FaceStack.Navigator
-    // headerMode="none"
-    >
+    headerMode="none">
       <FaceStack.Screen
         name="Face"
         component={FacebookDashboard}
         options={{
-
+          headerStyle: {
+            backgroundColor: '#FFF',
+          }
         }}
       />
-      {/* <FaceStack.Screen name="Youtube" component={YoutubeDashboard} /> */}
+
     </FaceStack.Navigator>
   );
 }
@@ -37,26 +43,52 @@ const Youtube = () => {
   );
 }
 
-function BottomTab() {
+const BottomTab = () => {
   return(
-    <BottomTabsStack.Navigator
-    >
+    <BottomTabsStack.Navigator>
       <BottomTabsStack.Screen name="Home" component={Face} />
-      <BottomTabsStack.Screen name="Settings" component={Youtube} />
+      <BottomTabsStack.Screen name="Settings" component={Face} />
     </BottomTabsStack.Navigator>
   );
 }
 
-// import { Container } from './styles';
-const RootStack = createStackNavigator();
+const TopTab = () => {
+  return(
+    <TopTabsStack.Navigator
+      tabBarOptions={{
+        activeTintColor: '#2979ff',
+        inactiveTintColor: '#424242'
+      }}
+    >
+      <TopTabsStack.Screen
+        name="Home"
+        component={Face}
+        options={{
+          tabBarLabel: ({ color }) =>
+            <MaterialIcons name="home" color={color} size={40} />,
+
+
+        }}
+      />
+      <TopTabsStack.Screen name="Settings" component={Face} />
+    </TopTabsStack.Navigator>
+  );
+}
+
+
 export default function Routes() {
   return (
-  <RootStack.Navigator headerMode="none">
+  <RootStack.Navigator >
     <RootStack.Screen
-      name="App"
-      component={BottomTab}
+      name="faceStack"
+      component={TopTab}
       options={{
-        animationEnabled: false
+        header: () => <HeaderFace />,
+        // headerRight: () => <ButtonsTop />,
+        headerTintColor:'#FFF',
+        headerStyle: {
+          backgroundColor: '#FFF'
+        },
       }}
     />
   </RootStack.Navigator>
