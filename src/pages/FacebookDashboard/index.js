e08b7200a8a67ef422935188321b3587a4b011e1
claudio-code <suporte2424@outlook.com>
2020-02-26 02:38:13 -0300

    adicionado o card superior ao dashboard

--- src/pages/FacebookDashboard/index.js
+++ src/pages/FacebookDashboard/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
-
+import CardTop from '../../components/Facebook/CardTop';
 export default function FacebookDashboard({ navigation }) {
 
   return (
-    <View />
+    <CardTop />
   );
 }
