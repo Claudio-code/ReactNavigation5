c1b09bedcc52d526f82126d49586c88b3cebde43
claudio-code <suporte2424@outlook.com>
2020-02-29 09:03:58 -0300

    adicionada card de stories

--- src/pages/FacebookDashboard/index.js
+++ src/pages/FacebookDashboard/index.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { Container } from './styles';
 import CardTop from "../../components/Facebook/CardTop";
+import CardStories from "../../components/Facebook/CardStories";
 
 export default function FacebookDashboard() {
 
   return (
     <Container>
       <CardTop />
-
+      <CardStories />
     </Container>
   );
 }
