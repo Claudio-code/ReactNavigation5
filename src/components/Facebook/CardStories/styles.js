83bfe2d70b61bcd47ea7b1d43adda1850bea21a8
claudio-code <suporte2424@outlook.com>
2020-02-29 09:02:15 -0300

    estilizado o card de stories

---
+++ src/components/Facebook/CardStories/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export const Container = styled.View`
+  background-color: #FFF;
+  margin-top: 4%;
+  padding: 3%;
+`;
+
+export const ImgStories = styled.Image`
+  width: 120px;
+  height: 220px;
+  border-radius: 25px;
+  margin-right: 13px;
+
+`;
+
+export const Icon = styled(MaterialCommunityIcons)`
+  bottom: 80%;
+  left: 10%;
+  background-color: #fff;
+  width: 17%;
+  border-radius: 50px;
+  color: #2979ff;
+`;
+
+
