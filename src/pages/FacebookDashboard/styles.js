de3c50a30b86bce4f5f32673a44ff2124833221f
claudio-code <suporte2424@outlook.com>
2020-02-29 09:05:00 -0300

    alterado estilo do dashboard do face para scrollview

--- src/pages/FacebookDashboard/styles.js
+++ src/pages/FacebookDashboard/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
-  
+export const Container = styled.ScrollView`
+  background-color: #e0e0e0;
+  height: 100%;
 `;
